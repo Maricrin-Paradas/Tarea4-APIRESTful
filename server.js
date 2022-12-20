e381582df36bdf10aa51979ebc2e194afbfad008
Maricrin Paradas <maricrinparadas@MacBook-Pro-de-Maricrin.local>
2022-12-19 21:32:48 -0300

    segundo commit

--- server.js
+++ server.js
@@ -1,53 +1,16 @@
-const router = require('express').Router();
-const { Producto, productos } = require('./productos.js');
+const express = require('express');
+const routerProductos = require('./routes/productos.js');
 
-router.get('/productos', (req, res) => {
-	res.json({ productos });
-});
+const PORT = 3000;
+const app = express();
 
-router.get('/productos/:id', (req, res) => {
-	let producto = Producto.productos.find(
-		producto => producto.id === Number(req.params.id)
-	);
-	if (producto) {
-		res.send(producto);
-	} else {
-		res.status(404).send({ error: 'Producto no encontrado' });
-	}
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-router.post('/productos', (req, res) => {
-	let { title, price, thumbnail } = req.body;
-	const producto = { title, price, thumbnail };
-	producto.id = Producto.productos.length + 1;
-	Producto.productos.push(producto);
-	res.send(Producto.productos);
-});
+app.use('/api', routerProductos);
 
-router.put('/productos/:id', (req, res) => {
-	let { title, price, thumbnail } = req.body;
-	let index = Producto.productos.findIndex(
-		producto => producto.id === Number(req.params.id)
-	);
-	if (index >= 0) {
-		Producto.productos[index] = { title, price, thumbnail };
-		Producto.productos[index].id = Number(req.params.id);
-		res.send(Producto.productos[index]);
-	} else {
-		res.status(404).send({ error: 'Producto no encontrado' });
-	}
-});
-
-router.delete('/productos/:id', (req, res) => {
-	let index = Producto.productos.findIndex(
-		producto => producto.id === Number(req.params.id)
-	);
-	if (index >= 0) {
-		Producto.productos.splice(index, 1);
-		res.send({ message: 'Producto eliminado' });
-	} else {
-		res.status(404).send({ error: 'Producto no encontrado' });
-	}
-});
-
-module.exports = router;
+const server = app.listen(PORT, () =>
+	console.log(`Server running on port ${PORT}`)
+);
+server.on('error', err => console.log(`Error: ${err}`));
